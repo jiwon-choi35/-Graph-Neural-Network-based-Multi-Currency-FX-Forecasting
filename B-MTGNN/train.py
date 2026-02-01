c159dadc32b1acdd09248492c3e4759ce378d08c
CHOI JIWON <jiwonnnnn35@gmail.com>
2026-02-02 02:49:35 +0900

    Update train.py

--- B-MTGNN/train.py
+++ B-MTGNN/train.py
@@ -8,9 +8,11 @@ import sys
 import os
 
 # Ensure scripts directory is in path for module imports
-script_dir = os.path.dirname(os.path.abspath(__file__))
-if script_dir not in sys.path:
-    sys.path.insert(0, script_dir)
+if 'google.colab' in str(get_ipython()):
+    # 현재 경로를 sys.path에 추가
+    current_path = os.getcwd()
+    if current_path not in sys.path:
+        sys.path.append(current_path)
 
 from net import gtnet
 from o_util import *
@@ -109,7 +111,10 @@ parser.add_argument('--num_split',type=int,default=1,help='number of splits for 
 parser.add_argument('--step_size',type=int,default=100,help='step_size')
 
 
-args = parser.parse_args()
+try:
+    args = parser.parse_args()
+except:
+    args = parser.parse_args(args=[])
 device = torch.device('cpu')
 torch.set_num_threads(3)
 
